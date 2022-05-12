a170b71d394a055253ef4943547e77cec3cf99c2
Daniel Eichman <D.Eichman@outlook.com>
2022-05-11 20:46:54 -0700

    Clean up stuff

--- src/main/java/org/zfz7/easy/TwoSum.java
+++ src/main/java/org/zfz7/easy/TwoSum.java
@@ -1,4 +1,4 @@
-package easy;
+package org.zfz7.easy;
 
 import java.util.HashMap;
 
@@ -6,11 +6,10 @@ public class TwoSum {
     // https://leetcode.com/problems/two-sum/
     public static int[] twoSum(int[] nums, int target) {
         //Key: num[pos], value: pos
-        HashMap values = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> values = new HashMap<>();
         for (int pos = 0; pos < nums.length; pos++) {
             if (values.containsKey(target - nums[pos])) {
-                int[] result = {(int) values.get(target - nums[pos]), pos};
-                return result;
+                return new int[]{values.get(target - nums[pos]), pos};
             } else {
                 values.put(nums[pos], pos);
             }
--- src/test/java/org/zfz7/easy/TwoSumTest.java
+++ src/test/java/org/zfz7/easy/TwoSumTest.java
@@ -1,4 +1,4 @@
-package easy;
+package org.zfz7.easy;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
